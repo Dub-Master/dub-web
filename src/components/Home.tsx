b031edc5ba4bb7a8707b36eea7a73907a524d735
Ilya Subkhankulov <ilya.subkhankulov@gmail.com>
2023-10-21 05:25:36 -0700

    added api call

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,51 @@
-import React, { FC } from "react";
+import React, { useState, FC } from "react";
 import LanguageSelect from "./LanguageSelect";
+import env from "react-dotenv";
+type PeopleType = {
+  id: number;
+  name: string;
+};
 
 const Home: FC = () => {
+  const [videoUrl, setVideoUrl] = useState<string>("");
+  const [language, setLanguage] = useState<PeopleType>({
+    id: 2,
+    name: "Spanish",
+  });
+
+  const submitJob = (videoUrl: string) => {
+    console.log("submitting job");
+
+    console.log({ api: env.API_URL });
+    const url = new URL(`${env.API_URL}/jobs`);
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ input_url: videoUrl }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event.target.value);
+    setVideoUrl(event.target.value);
+  }
+
   return (
     <div className="flex flex-1 flex-col justify-center items-center">
       <input
         type="text"
         name="videoUrl"
+        onChange={(e) => handleChange(e)}
         className="w-1/3 text-lg p-4 rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
         placeholder="Enter Youtube Link"
       />
-      <LanguageSelect />
+      <LanguageSelect Language={language} setLanguage={setLanguage} />
       <button
+        onClick={() => submitJob(videoUrl)}
         type="button"
         className="mt-4 rounded-md bg-white px-3.5 py-2.5 text-lg font-semibold text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
       >
--- src/components/LanguageSelect.tsx
+++ src/components/LanguageSelect.tsx
@@ -3,27 +3,47 @@ import { Fragment, useState } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
-const people = [
+type PeopleType = {
+  id: number;
+  name: string;
+};
+
+const people: PeopleType[] = [
   { id: 1, name: "English" },
   { id: 2, name: "Spanish" },
   { id: 3, name: "German" },
   { id: 4, name: "French" },
 ];
 
+type LanguageSelectProps = {
+  Language: PeopleType;
+  setLanguage: (language: PeopleType) => void;
+};
+
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-const LanguageSelect: FC = () => {
-  const [selected, setSelected] = useState(people[1]);
+const LanguageSelect: React.FC<LanguageSelectProps> = ({
+  Language,
+  setLanguage,
+}) => {
+  // const LanguageSelect: FC = ({
+  //   Language,
+  //   setLanguage,
+  // }: {
+  //   Language: PeopleType;
+  //   setLanguage: (language: PeopleType) => void;
+  // }) => {
+  setLanguage(people[1]);
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={Language} onChange={setLanguage}>
       {({ open }) => (
         <>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 text-lg">
-              <span className="block truncate">{selected.name}</span>
+              <span className="block truncate">{Language.name}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon
                   className="h-5 w-5 text-gray-400"
@@ -46,7 +66,7 @@ const LanguageSelect: FC = () => {
                     className={({ active }) =>
                       classNames(
                         active ? "bg-indigo-600 text-white" : "text-gray-500",
-                        "relative cursor-default select-none py-2 pl-3 pr-9",
+                        "relative cursor-default select-none py-2 pl-3 pr-9"
                       )
                     }
                     value={person}
@@ -56,7 +76,7 @@ const LanguageSelect: FC = () => {
                         <span
                           className={classNames(
                             selected ? "font-semibold" : "font-normal",
-                            "block truncate",
+                            "block truncate"
                           )}
                         >
                           {person.name}
@@ -66,7 +86,7 @@ const LanguageSelect: FC = () => {
                           <span
                             className={classNames(
                               active ? "text-white" : "text-indigo-600",
-                              "absolute inset-y-0 right-0 flex items-center pr-2",
+                              "absolute inset-y-0 right-0 flex items-center pr-2"
                             )}
                           >
                             <CheckIcon className="h-5 w-5" aria-hidden="true" />
